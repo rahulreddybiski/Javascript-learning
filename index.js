c7950f357975b204950c2fe293b68489f19d9cfa
rahulreddybiski <rahulreddy.biski@gmail.com>
2022-07-05 14:17:32 +0530

    Day1_learnings

---
+++ index.js
@@ -0,0 +1,19 @@
+//comments can make code readable
+//welcome to 30DaysOfJavaScript
+
+/*
+    comments can make code readable, 
+    easy to reuse and informative
+*/
+
+let name='Rahul';
+let height=5.11;
+let male=true;
+let married=null;
+let jack;
+
+console.log(typeof(name));
+console.log(typeof(jack));
+
+let myName='Rahulreddy', age=23;
+console.log(myName , age)
