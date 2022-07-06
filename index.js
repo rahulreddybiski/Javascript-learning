fb5df42927b9fb21a935ac5827d270a9f9509207
rahulreddybiski <rahulreddy.biski@gmail.com>
2022-07-06 20:14:17 +0530

    day2 learnings

--- index.js
+++ index.js
@@ -5,15 +5,39 @@
     comments can make code readable, 
     easy to reuse and informative
 */
+// -----Day1-----
+// let name='Rahul';
+// let height=5.11;
+// let male=true;
+// let married=null;
+// let jack;
 
-let name='Rahul';
-let height=5.11;
-let male=true;
-let married=null;
-let jack;
+// console.log(typeof(name));
+// console.log(typeof(jack));
 
-console.log(typeof(name));
-console.log(typeof(jack));
+// let myName='Rahulreddy', age=23;
+// console.log(myName , age)
 
-let myName='Rahulreddy', age=23;
-console.log(myName , age)
+/* const now = new Date();
+console.log(now)
+console.log(now.getFullYear())
+console.log(now.getTime())
+
+
+console.log('10'===10)
+console.log(parseInt('9.8')==10)
+
+// Area of the traingle
+
+let base= prompt("enter the base")
+let hieight=prompt("enter the height")
+
+let area=0.5*base*hieight;
+
+console.log("area of the trainsge id : ",area) */
+/* 
+let firstName= 'Rahulreddy'
+let lastName= 'Biski'
+firstName>lastName? console.log("firstname is greater than lastname"):console.log("lastname is greater than firstname")
+ */
+
